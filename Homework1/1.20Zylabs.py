849ea28b6de374317d549ff3a8c1c87c91c677e6
gustavo-sanchez1 <77949975+gustavo-sanchez1@users.noreply.github.com>
2021-02-10 11:05:08 -0600

    Add files via upload

---
+++ Homework1/1.20Zylabs.py
@@ -0,0 +1,9 @@
+print('Enter integer:')  # get user's input
+user_num = int(input())
+print('You entered:', user_num)
+print(user_num, 'squared is', user_num*user_num)  # square the numbers
+print('And', user_num, 'cubed is', user_num*user_num*user_num, '!!')  # cube the numbers
+print('Enter another integer:')  # enter user's input and display them
+user_num2 = int(input())
+print(user_num, '+', user_num2, 'is', user_num+user_num2)
+print(user_num, '*', user_num2, 'is', user_num*user_num2)
